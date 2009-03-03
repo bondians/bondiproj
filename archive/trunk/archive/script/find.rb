f68bf576972625cedf7575a6f27f89fcde56a0ca
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-03 18:44:23 +0000

    find changes
    git-svn-id: svn://localhost/bondiproj@19 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/script/find.rb
+++ archive/trunk/archive/script/find.rb
@@ -42,33 +42,35 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
                 ## Try to find old archive _id
                 tag_text = tag.find{|t| t[:id]==:TXXX}
                 attributes[:archive_number] = tag_text[:text] if tag_text
-                break "song in DB" if Song.find_by_archive_number(attributes[:archive_number])
-                
-                
-                #### Try to find each of the rest of the important fields
-                ##Artist
-                artist_tag = tag.artist ? Iconv.conv('UTF-8', 'LATIN1', tag.artist) : "<no artist>"
-                artist = Artist.find_by_name(artist_tag) || Artist.new({:name=>artist_tag})
-                artist.save if artist.new_record?
-                attributes[:artist] = artist
-                
-                ##Genre
-                genre_tag  =  tag.genre ? Iconv.conv('UTF-8', 'LATIN1', tag.genre) : "Unclassifiable"
-                genre = Genre.find_by_name(genre_tag) || Genre.new({:name=>genre_tag})
-                genre.save if genre.new_record?
-                attributes[:genre] = genre
-                
-                ##Album
-                album_tag  =  tag.album ? Iconv.conv('UTF-8', 'LATIN1', tag.album) : "<no album>"
-                album = Album.find_by_name(album_tag) || Album.new({:name=>album_tag, :genre=> genre})
-                album.save if album.new_record?
-                attributes[:album] = album
-                
-                song = Song.new(attributes)
-                if song.save
-                    puts "Saved MP3 Titled #{attributes[:title]}"
+                if !!!Song.find_by_archive_number(attributes[:archive_number])
+                    
+                    #### Try to find each of the rest of the important fields
+                    ##Artist
+                    artist_tag = tag.artist ? Iconv.conv('UTF-8', 'LATIN1', tag.artist) : "<no artist>"
+                    artist = Artist.find_by_name(artist_tag) || Artist.new({:name=>artist_tag})
+                    artist.save if artist.new_record?
+                    attributes[:artist] = artist
+                    
+                    ##Genre
+                    genre_tag  =  tag.genre ? Iconv.conv('UTF-8', 'LATIN1', tag.genre) : "Unclassifiable"
+                    genre = Genre.find_by_name(genre_tag) || Genre.new({:name=>genre_tag})
+                    genre.save if genre.new_record?
+                    attributes[:genre] = genre
+                    
+                    ##Album
+                    album_tag  =  tag.album ? Iconv.conv('UTF-8', 'LATIN1', tag.album) : "<no album>"
+                    album = Album.find_by_name(album_tag) || Album.new({:name=>album_tag, :genre=> genre})
+                    album.save if album.new_record?
+                    attributes[:album] = album
+                    
+                    song = Song.new(attributes)
+                    if song.save
+                        puts "Saved MP3 Titled #{attributes[:title]}"
+                    else
+                        puts "Saved MP3 Titled #{attributes[:title]}"
+                    end
                 else
-                    puts "Saved MP3 Titled #{attributes[:title]}"
+                    puts "Song in DB #{attributes[:title]}"
                 end
  
             when "m4a"
