07732c15fc43f84971e3c93217eba01be059f92e
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-04 06:30:17 +0000

    goldberg migration
    git-svn-id: svn://localhost/bondiproj@67 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/models/song.rb
+++ archive/trunk/archive/app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   belongs_to :songtype
+  validates_presence_of :songtype
   
   def self.search(search, page)
     paginate :per_page => 100, :page => page,
