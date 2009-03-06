4e1253109b4bdcab0efdc7439d3bdfe6e6c728a2
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-06 21:34:16 +0000

    testing 'blank index'
    git-svn-id: svn://localhost/bondiproj@146 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/config/environments/development.rb
+++ archive/trunk/archive/config/environments/development.rb
@@ -17,3 +17,5 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 
 require 'will_paginate'
+
+Mime::Type.register 'audio/mpeg', :mp3
