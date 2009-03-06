7c7f1ccf0e02687e9b3e30207695d8f5c8292dcc
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-06 06:17:36 +0000

    adding tabs and such
    git-svn-id: svn://localhost/bondiproj@110 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/albums_controller.rb
+++ archive/trunk/archive/app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < ApplicationController
   # GET /albums
   # GET /albums.xml
   def index
-    @albums = Album.find(:all)
+    @albums = Album.find(:all :include=>:genre)
 
     respond_to do |format|
       format.html # index.html.erb
