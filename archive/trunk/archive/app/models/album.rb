31788bdb1e02e72faae217cc703dc30c73f1b0b6
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-06 06:44:46 +0000

    added more indexing
    git-svn-id: svn://localhost/bondiproj@113 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/models/album.rb
+++ archive/trunk/archive/app/models/album.rb
@@ -2,4 +2,10 @@ class Album < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :songs
+  
+  define_index do
+    indexes :name, :sortable => true
+  end
+  
+  
 end
--- archive/trunk/archive/app/models/artist.rb
+++ archive/trunk/archive/app/models/artist.rb
@@ -1,4 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :albums
+  
+  define_index do
+    indexes :name, :sortable => true
+  end
+  
 end
