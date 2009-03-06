b758617aa8394d7094f4ed040734a7e7e041d53c
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-06 23:33:02 +0000

    testing 'blank index'
    git-svn-id: svn://localhost/bondiproj@174 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/songviews_controller.rb
+++ archive/trunk/archive/app/controllers/songviews_controller.rb
@@ -3,6 +3,5 @@ class SongviewsController < ApplicationController
   def stream_one_song
     song = Song.find params[:id]
     send_file song.file, :type => song.songtype.mime_type, :disposition => :inline
-    die
   end
 end
