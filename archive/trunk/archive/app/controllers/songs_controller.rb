e02edcaa0678da10fe54aec9d18e94ab38a4b0fb
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-06 21:23:54 +0000

    testing 'blank index'
    git-svn-id: svn://localhost/bondiproj@141 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/songs_controller.rb
+++ archive/trunk/archive/app/controllers/songs_controller.rb
@@ -11,7 +11,8 @@ class SongsController < ApplicationController
   
   def stream_one_song
    song = Song.find params[:id]
-   send_file song.file, :type => song.songtype.mime_type, :disposition => :inline 
+   #send_data(@media.data, :type => "audio/mpeg", :filename => "media-#{@media.id}.mp3", :disposition => "inline")
+   send_file song.file, :type => "audio/mpeg", :filename=> File.basepath.song.file, :disposition => :inline 
   end
 
 
