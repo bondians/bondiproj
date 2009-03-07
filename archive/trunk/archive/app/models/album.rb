e742f5a75a7aca9d90bcb1986914802a93cab928
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-07 03:37:39 +0000

    testing embed
    git-svn-id: svn://localhost/bondiproj@178 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/models/album.rb
+++ archive/trunk/archive/app/models/album.rb
@@ -4,7 +4,7 @@ class Album < ActiveRecord::Base
   has_many :songs
   
   define_index do
-    indexes name, :sortable => true
+    indexes :name, :sortable => true
     indexes artist.name, :as => :artist, :sortable => true
     indexes genre.name, :as => :genre, :sortable => true
   end
