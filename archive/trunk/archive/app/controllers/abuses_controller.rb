77b6ccf3a58cecc4f9c09e3407c2c38be3dc10f8
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-09 21:22:01 +0000

    abuses
    git-svn-id: svn://localhost/bondiproj@202 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

---
+++ archive/trunk/archive/app/controllers/abuses_controller.rb
@@ -0,0 +1,80 @@
+class AbusesController < ApplicationController
+  # GET /abuses
+  # GET /abuses.xml
+  def index
+    Abuse.first :offset=>rand(Abuse.all.length)
+  end
+
+  # GET /abuses/1
+  # GET /abuses/1.xml
+  def show
+    @abuse = Abuse.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @abuse }
+    end
+  end
+
+  # GET /abuses/new
+  # GET /abuses/new.xml
+  def new
+    @abuse = Abuse.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @abuse }
+    end
+  end
+
+  # GET /abuses/1/edit
+  def edit
+    @abuse = Abuse.find(params[:id])
+  end
+
+  # POST /abuses
+  # POST /abuses.xml
+  def create
+    @abuse = Abuse.new(params[:abuse])
+
+    respond_to do |format|
+      if @abuse.save
+        flash[:notice] = 'Abuse was successfully created.'
+        format.html { redirect_to(@abuse) }
+        format.xml  { render :xml => @abuse, :status => :created, :location => @abuse }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @abuse.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /abuses/1
+  # PUT /abuses/1.xml
+  def update
+    @abuse = Abuse.find(params[:id])
+
+    respond_to do |format|
+      if @abuse.update_attributes(params[:abuse])
+        flash[:notice] = 'Abuse was successfully updated.'
+        format.html { redirect_to(@abuse) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @abuse.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /abuses/1
+  # DELETE /abuses/1.xml
+  def destroy
+    @abuse = Abuse.find(params[:id])
+    @abuse.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(abuses_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
--- archive/trunk/archive/config/routes.rb
+++ archive/trunk/archive/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :abuses
+
   
   map.resources :sonviews
   
