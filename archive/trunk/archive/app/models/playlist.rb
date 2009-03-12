8900c34e2c2b2559731393ae2923e579b6430273
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-12 23:13:18 +0000

    playlist stuff
    git-svn-id: svn://localhost/bondiproj@328 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/models/playlist.rb
+++ archive/trunk/archive/app/models/playlist.rb
@@ -2,6 +2,7 @@ class Playlist < ActiveRecord::Base
 
   belongs_to :user, :class_name=>"Goldberg::User", :foreign_key => :user_id
   has_many :plentries
+  has_many :songs, :through => :plentries
   
   attr_accessible :name
 end
