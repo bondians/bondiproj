00f2b5dbdfaf85d81bb711619456c9756c4b2a85
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-13 06:20:59 +0000

    draw_songs method
    git-svn-id: svn://localhost/bondiproj@358 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/helpers/selections_dispatch_helper.rb
+++ archive/trunk/archive/app/helpers/selections_dispatch_helper.rb
@@ -1,2 +1,20 @@
 module SelectionsDispatchHelper
+  ## If you want to add a button, go to the controller for this helper, and add it to dispatch
+  ## Then add the button to the _wrapper view with some sort of unique value
+  ## Call with an already sorted array of songs
+  ## Variable number of :symbols for any column you want _NOT_ displayed
+  ## available for nodraw:
+  # :checkbox  <= also disables all buttons
+  # :title
+  # :artist
+  # :album
+  # :genre
+  # :size
+  # :type
+  # :get
+  
+    def draw_songs(songs, *nodraws)
+      render :template => "wrapper", :locals => {:songs => songs, :nodraws => nodraws}
+    end
+    
 end
