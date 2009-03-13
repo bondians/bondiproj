3eee72458ecf2d5636f911cbbb8cc87ce3db29a4
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-13 00:48:11 +0000

    playlist loader
    git-svn-id: svn://localhost/bondiproj@330 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/script/playlist_loader.rb
+++ archive/trunk/archive/script/playlist_loader.rb
@@ -4,11 +4,41 @@ require File.expand_path(__FILE__ + "/../../config/environment")
 
 data = IO.readlines("#{RAILS_ROOT}/oldplaylists.txt")
 
+playlists = Playlist.all :include=>:user
+
+users = Goldberg::User.all
+
 data.each do |line|
   (archive, list, title) = line.split("|")
   (listuser, listname) = list.split(".")
   listname ||= "Favorites"
   
+  if listname == "nidor"
+    listuser = "nidor"
+  end
+  
+  
+  song = Song.find_by_archive_number archive
+  if song
+    user = users.find{|u| u.name == listuser}
+    if user  
+      playlist = playlists.find{|p| p.user.name == listuser && p.name == listname}
+      unless playlist
+        playlist = Playlist.new
+        playlist.user = user
+        playlist.name = listnam
+        playlist.save
+        playlists.unshift(playlist)
+      end
+      pl = playlist.plentry.build
+      pl.song = song
+      pl.save
+    else
+      puts "no user: #{line}"
+    end
+  else
+    puts "no song: #{line}"
+  end
 end
 
 # 192264|cayuse.country|Tiny Broken Heart
