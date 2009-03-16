e6da06045d09b401258799401776c7a15e0c582d
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-16 05:08:48 +0000

    adding remove to playlists
    git-svn-id: svn://localhost/bondiproj@403 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/selections_dispatch_controller.rb
+++ archive/trunk/archive/app/controllers/selections_dispatch_controller.rb
@@ -3,18 +3,12 @@ class SelectionsDispatchController < ApplicationController
     ## i need these, make sure you pull yours out safely
     songs = params[:songs]
     playlist = Playlist.find(params[:playlist][:id]) if params[:playlist]
-    debugger
-    1
-    1
     case params[:submit]
     when "Add to Playlist"
       Playlist.add_songs_to_playlist(songs, playlist)
       redirect_to(playlist)
 
     when "Remove from Playlist"
-      debugger
-      1
-      1
       Playlist.remove_songs_from_playlist(songs, playlist)
       redirect_to(playlist)
     end
