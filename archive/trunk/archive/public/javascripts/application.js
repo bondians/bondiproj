2e2607041eff90b1318ad6eb6f0bf0cea42cb836
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-17 01:53:43 +0000

    working with jquery
    git-svn-id: svn://localhost/bondiproj@436 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/public/javascripts/application.js
+++ archive/trunk/archive/public/javascripts/application.js
@@ -1,32 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(
-	function () {
-		$('a.closeEl').bind('click', toggleContent);
-		$('div.groupWrapper').Sortable(
-			{
-				accept: 'groupItem',
-				helperclass: 'sortHelper',
-				activeclass : 	'sortableactive',
-				hoverclass : 	'sortablehover',
-				handle: 'tr.cssAlternating1',
-				tolerance: 'pointer',
-				onChange : function(ser)
-				{
-				},
-				onStart : function()
-				{
-					$.iAutoscroller.start(this, document.getElementsByTagName('body'));
-				},
-				onStop : function()
-				{
-					$.iAutoscroller.stop();
-				}
-			}
-		);
-	}
-);
 
 function toggleAll(name)
 {
