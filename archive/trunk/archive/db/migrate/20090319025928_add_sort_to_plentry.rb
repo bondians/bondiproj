6cc1212952f5aff06071020a76e3a38c398f41ab
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-19 05:23:37 +0000

    migration
    git-svn-id: svn://localhost/bondiproj@450 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/db/migrate/20090319025928_add_sort_to_plentry.rb
+++ archive/trunk/archive/db/migrate/20090319025928_add_sort_to_plentry.rb
@@ -1,9 +1,9 @@
 class AddSortToPlentry < ActiveRecord::Migration
   def self.up
-    add_column :plentries, :sort, :integer
+    add_column :plentries, :idx, :integer
   end
 
   def self.down
-    remove_column :plentries, :sort
+    remove_column :plentries, :idx
   end
 end
