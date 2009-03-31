2550acbbb1785cbf936f56bcbaf515cbab9ba4ef
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-03-31 20:45:43 +0000

    minor changes
    git-svn-id: svn://localhost/bondiproj@492 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- sanford/trunk/app/controllers/submissions_controller.rb
+++ sanford/trunk/app/controllers/submissions_controller.rb
@@ -1,6 +1,5 @@
 class SubmissionsController < ApplicationController
-  # GET /submissions
-  # GET /submissions.xml
+
   def index
     if Goldberg.user.role.cache[:credentials].permission_ids.include?(6)
       @submissions = Submission.all
@@ -9,78 +8,55 @@ class SubmissionsController < ApplicationController
     end
   end
 
-  # GET /submissions/1
-  # GET /submissions/1.xml
   def show
     @submission = Submission.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @submission }
+    unless Goldberg.user.role.cache[:credentials].permission_ids.include?(6) || Goldberg.user == @submission.user
+      flash[:error] = "You Cannot View this submission."
+      redirect_to(submissions_url)
     end
   end
 
-  # GET /submissions/new
-  # GET /submissions/new.xml
   def new
     @submission = Submission.new
     @submission.user = Goldberg.user
   end
 
-  # GET /submissions/1/edit
   def edit
     @submission = Submission.find(params[:id])
-    unless Goldberg.user.role.cache[:credentials].permission_ids.include?(6) || Goldberg.user.submissions.include?(@submission)
-      flash[:error] = "You cannot edit someone elses submission"
+    unless Goldberg.user.role.cache[:credentials].permission_ids.include?(6) || Goldberg.user == @submission.user
+      flash[:error] = "You cannot Edit this submission."
       redirect_to(submissions_url)
     end
   end
 
-  # POST /submissions
-  # POST /submissions.xml
   def create
     @submission = Submission.new(params[:submission])
     @submission.user = Goldberg.user
 
-    respond_to do |format|
-      if @submission.save
-        flash[:notice] = 'Submission was successfully created.'
-        format.html { redirect_to(@submission) }
-        format.xml  { render :xml => @submission, :status => :created, :location => @submission }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @submission.errors, :status => :unprocessable_entity }
-      end
+    if @submission.save
+      flash[:notice] = 'Submission was successfully created.'
+      redirect_to(@submission)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /submissions/1
-  # PUT /submissions/1.xml
   def update
     params[:submission][:existing_item_attributes] ||= {}
     @submission = Submission.find(params[:id])
 
-    respond_to do |format|
-      if @submission.update_attributes(params[:submission])
-        flash[:notice] = 'Submission was successfully updated.'
-        format.html { redirect_to(@submission) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @submission.errors, :status => :unprocessable_entity }
-      end
+    if @submission.update_attributes(params[:submission])
+      flash[:notice] = 'Submission was successfully updated.'
+      redirect_to(@submission)
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /submissions/1
-  # DELETE /submissions/1.xml
   def destroy
     @submission = Submission.find(params[:id])
     @submission.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(submissions_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(submissions_url)
   end
+  
 end
