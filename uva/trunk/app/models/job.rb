76a9ec04895cd152bd3d566160c337f1e3e1acfd
tindrum <tindrum@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-12 01:41:04 +0000

    still working, prepping for yaml import
    git-svn-id: svn://localhost/bondiproj@784 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- uva/trunk/app/models/job.rb
+++ uva/trunk/app/models/job.rb
@@ -8,6 +8,8 @@ class Job < ActiveRecord::Base
   
   accepts_nested_attributes_for :workflows, :allow_destroy => true, :reject_if => proc { |attributes| attributes["step_needed"] == "0" } 
   
+  #named_scope :not_shipped, :conditions => self.shipping.completed == false
+  
   def after_save
     #params.each do |key, value|
     
@@ -16,5 +18,9 @@ class Job < ActiveRecord::Base
   
   def before_delete
     
+  end
+  
+  def shipping
+     Workflow.find(:all, :conditions => {:job_id => self, :name => 'Ship' } )
   end
 end
