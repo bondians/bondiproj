26565c1a97a25e42feda807d44bce2e8daa8c154
tindrum <tindrum@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-13 19:32:46 +0000

    minor tweak to workflow simple status display
    git-svn-id: svn://localhost/bondiproj@794 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- uva/trunk/app/helpers/jobs_helper.rb
+++ uva/trunk/app/helpers/jobs_helper.rb
@@ -1,9 +1,9 @@
 module JobsHelper
   def workflow_step_display(job)
-    stepString = String.new
+    stepString = "["
     job.workflow_steps_simple.each  do |wf|  
       stepString << (wf.completed ? wf.name.slice(0..0).downcase : wf.name.slice(0..0))
     end
-    stepString
+    stepString << "]"
   end
 end
