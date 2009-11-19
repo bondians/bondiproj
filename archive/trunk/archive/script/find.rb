9bac53bff958e3275fb0710416d570be12b8d227
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-19 18:41:28 +0000

    finder
    git-svn-id: svn://localhost/bondiproj@838 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/script/find.rb
+++ archive/trunk/archive/script/find.rb
@@ -60,31 +60,86 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
   Find.find(options.path) do |path|
       ########### This currently sucks, becaus i eventually want to modify files.. however, this is no problem
       ########### Currently as that is not implemented it <should> be fixd when file modding becomes possible
-      next if (!options.full && (@lastrun.started < File.ctime(path)))
-      if FileTest.file?(path) && !path.match(".AppleDouble")
-        kind = path.split(".")
-        case kind.last.downcase
-            when "mp3"
-              tag = ID3Lib::Tag.new(path)
-              
-                ##Build a song object, while working with the rest
-                attributes = DEFAULTS
-                attributes[:file] = Iconv.conv('UTF-8', 'LATIN1', path)
-                ## Shortcircuit if its already present
-                if !@songs.find{|s| s.file == attributes[:file]}
-                    attributes[:title] = !!tag.title ? Iconv.conv('UTF-8', 'LATIN1', tag.title) : "<no title>"
-                    attributes[:size] = File.size(path)
-                    attributes[:year] = tag.year
-                    attributes[:track] = tag.track ? tag.track.split("/").first.to_i : nil
-                    attributes[:songtype] = @types.find{|t| t.identifier == kind.last.downcase}
-                
-                    ## Try to find old archive _id
-                    tag_text = tag.find{|t| t[:id]==:TXXX}
-                    attributes[:archive_number] = tag_text[:text] if tag_text
+      if (options.full || (@lastrun.started < File.ctime(path)))
+          if FileTest.file?(path) && !path.match(".AppleDouble")
+            kind = path.split(".")
+            case kind.last.downcase
+                when "mp3"
+                  tag = ID3Lib::Tag.new(path)
+                  
+                    ##Build a song object, while working with the rest
+                    attributes = DEFAULTS
+                    attributes[:file] = Iconv.conv('UTF-8', 'LATIN1', path)
+                    ## Shortcircuit if its already present
+                    if !@songs.find{|s| s.file == attributes[:file]}
+                        attributes[:title] = !!tag.title ? Iconv.conv('UTF-8', 'LATIN1', tag.title) : "<no title>"
+                        attributes[:size] = File.size(path)
+                        attributes[:year] = tag.year
+                        attributes[:track] = tag.track ? tag.track.split("/").first.to_i : nil
+                        attributes[:songtype] = @types.find{|t| t.identifier == kind.last.downcase}
                     
+                        ## Try to find old archive _id
+                        tag_text = tag.find{|t| t[:id]==:TXXX}
+                        attributes[:archive_number] = tag_text[:text] if tag_text
+                        
+                        #### Try to find each of the rest of the important fields
+                        ##Artist
+                        artist_tag = tag.artist ? Iconv.conv('UTF-8', 'LATIN1', tag.artist) : "<no artist>"
+                        artist = @artists.find{|a| a.name == artist_tag} || Artist.new({:name=>artist_tag})
+                        if artist.new_record?
+                            artist.save
+                            @artists.unshift(artist)
+                        end
+                        attributes[:artist] = artist
+                        
+                        ##Genre
+                        genre_tag  =  tag.genre ? Iconv.conv('UTF-8', 'LATIN1', tag.genre) : "Unclassifiable"
+                        if genre_tag.match(/^\(\d+\)$/)
+                            num = genre_tag.gsub("(","").gsub(")","").to_i
+                            genre_tag = Tagger::GENRES[num]
+                        end
+                        genre = @genres.find{|g| g.name == genre_tag} || Genre.new({:name=>genre_tag})
+                        if genre.new_record?
+                            genre.save
+                            @genres.unshift(genre)
+                        end
+                        attributes[:genre] = genre
+                        
+                        ##Album
+                        album_tag  =  tag.album ? Iconv.conv('UTF-8', 'LATIN1', tag.album) : "<no album>"
+                        album = @albums.find{|a| a.name == album_tag} || Album.new({:name=>album_tag, :genre=> genre})
+                        if album.new_record?
+                            album.save
+                            @albums.unshift(album)
+                        end
+                        attributes[:album] = album
+                        
+                        song = Song.new(attributes)
+                        if song.save
+                            puts "Saved MP3 Titled #{attributes[:title]}"
+                            @songs.unshift(song)
+                        else
+                            puts "Failed to save MP3 Titled #{attributes[:title]}"
+                        end
+                    else
+                        puts "Song in DB #{attributes[:title]}"
+                    end
+        
+                when "m4a", "m4p"
+                  tag = MP4Info.open(path)
+                  ##Build a song object, while working with the rest
+                  attributes = DEFAULTS
+                  attributes[:file] = Iconv.conv('UTF-8', 'LATIN1', path)
+                  if !@songs.find{|s| s.file == attributes[:file]}
+                    attributes[:title] = !!tag.NAM ? Iconv.conv('UTF-8', 'LATIN1', tag.NAM) : "<no title>"
+                    attributes[:size] = File.size(path)
+                    attributes[:year] = tag.DAY.to_i
+                    attributes[:track] = tag.TRKN ? tag.TRKN.first : nil
+            	attributes[:songtype] = @types.find{|t| t.identifier == kind.last.downcase}
+        
                     #### Try to find each of the rest of the important fields
                     ##Artist
-                    artist_tag = tag.artist ? Iconv.conv('UTF-8', 'LATIN1', tag.artist) : "<no artist>"
+                    artist_tag = tag.ART ? Iconv.conv('UTF-8', 'LATIN1', tag.ART) : "<no artist>"
                     artist = @artists.find{|a| a.name == artist_tag} || Artist.new({:name=>artist_tag})
                     if artist.new_record?
                         artist.save
@@ -93,11 +148,7 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
                     attributes[:artist] = artist
                     
                     ##Genre
-                    genre_tag  =  tag.genre ? Iconv.conv('UTF-8', 'LATIN1', tag.genre) : "Unclassifiable"
-                    if genre_tag.match(/^\(\d+\)$/)
-                        num = genre_tag.gsub("(","").gsub(")","").to_i
-                        genre_tag = Tagger::GENRES[num]
-                    end
+                    genre_tag  =  tag.GNRE ? Iconv.conv('UTF-8', 'LATIN1', tag.GNRE) : "Unclassifiable"
                     genre = @genres.find{|g| g.name == genre_tag} || Genre.new({:name=>genre_tag})
                     if genre.new_record?
                         genre.save
@@ -106,7 +157,7 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
                     attributes[:genre] = genre
                     
                     ##Album
-                    album_tag  =  tag.album ? Iconv.conv('UTF-8', 'LATIN1', tag.album) : "<no album>"
+                    album_tag  =  tag.ALB ? Iconv.conv('UTF-8', 'LATIN1', tag.ALB) : "<no album>"
                     album = @albums.find{|a| a.name == album_tag} || Album.new({:name=>album_tag, :genre=> genre})
                     if album.new_record?
                         album.save
@@ -118,71 +169,21 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
                     if song.save
                         puts "Saved MP3 Titled #{attributes[:title]}"
                         @songs.unshift(song)
+                        @currun.added += 1
+                        @currun.save
                     else
                         puts "Failed to save MP3 Titled #{attributes[:title]}"
                     end
-                else
+                  else
                     puts "Song in DB #{attributes[:title]}"
-                end
- 
-            when "m4a", "m4p"
-              tag = MP4Info.open(path)
-              ##Build a song object, while working with the rest
-              attributes = DEFAULTS
-              attributes[:file] = Iconv.conv('UTF-8', 'LATIN1', path)
-              if !@songs.find{|s| s.file == attributes[:file]}
-                attributes[:title] = !!tag.NAM ? Iconv.conv('UTF-8', 'LATIN1', tag.NAM) : "<no title>"
-                attributes[:size] = File.size(path)
-                attributes[:year] = tag.DAY.to_i
-                attributes[:track] = tag.TRKN ? tag.TRKN.first : nil
-		attributes[:songtype] = @types.find{|t| t.identifier == kind.last.downcase}
-
-                #### Try to find each of the rest of the important fields
-                ##Artist
-                artist_tag = tag.ART ? Iconv.conv('UTF-8', 'LATIN1', tag.ART) : "<no artist>"
-                artist = @artists.find{|a| a.name == artist_tag} || Artist.new({:name=>artist_tag})
-                if artist.new_record?
-                    artist.save
-                    @artists.unshift(artist)
-                end
-                attributes[:artist] = artist
-                
-                ##Genre
-                genre_tag  =  tag.GNRE ? Iconv.conv('UTF-8', 'LATIN1', tag.GNRE) : "Unclassifiable"
-                genre = @genres.find{|g| g.name == genre_tag} || Genre.new({:name=>genre_tag})
-                if genre.new_record?
-                    genre.save
-                    @genres.unshift(genre)
-                end
-                attributes[:genre] = genre
-                
-                ##Album
-                album_tag  =  tag.ALB ? Iconv.conv('UTF-8', 'LATIN1', tag.ALB) : "<no album>"
-                album = @albums.find{|a| a.name == album_tag} || Album.new({:name=>album_tag, :genre=> genre})
-                if album.new_record?
-                    album.save
-                    @albums.unshift(album)
-                end
-                attributes[:album] = album
-                
-                song = Song.new(attributes)
-                if song.save
-                    puts "Saved MP3 Titled #{attributes[:title]}"
-                    @songs.unshift(song)
-                else
-                    puts "Failed to save MP3 Titled #{attributes[:title]}"
-                end
-              else
-                puts "Song in DB #{attributes[:title]}"
-              end
-        else
-            puts "unknown"
-            #puts "#{path} is some other shit!!"
-        end
-      else
-        puts "."
-        @currun.added += 1
-        @currun.save
+                  end
+            else
+                puts "unknown"
+                #puts "#{path} is some other shit!!"
+            end
+          else
+            puts "."
+          end
     end
   end
     
