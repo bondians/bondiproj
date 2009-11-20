dd629c83df2496e3dd2bb17b42c5813556076b97
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-20 02:11:04 +0000

    find
    git-svn-id: svn://localhost/bondiproj@852 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/script/find.rb
+++ archive/trunk/archive/script/find.rb
@@ -189,7 +189,10 @@ DEFAULTS = {:volume => 0.7, :fade_duration => -1, :fade_in => true}
                         if !new 
                             album = choices.find { |a| a.artist == artist }
                             while !album
-                                puts "\n Album Doesn't match artist plese select an action\n"
+                                puts "\n Album Doesn't match artist plese select an action for:
+Title:  #{attribues[:title]}
+Artist: #{artist.name}
+File:   #{attributes[:file]}"
                                 choices.each_index { |i| puts "enter #{i} to select #{choices[i].name} by #{choices[i].artist.name}\n" }
                                 puts "enter n for new\n"
                                 a = gets.upcase.chomp
