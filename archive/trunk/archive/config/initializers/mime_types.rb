9f403f33dc285d4c93cfc5b657341bce50bb2aac
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-23 19:17:28 +0000

    images
    git-svn-id: svn://localhost/bondiproj@901 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/config/initializers/mime_types.rb
+++ archive/trunk/archive/config/initializers/mime_types.rb
@@ -6,7 +6,7 @@
 
     Mime::Type.register "audio/x-mpegurl", :m3u
     Mime::Type.register "image/jpeg", :jpg
-    Mime::Type.register "image/png", :jpg
+    Mime::Type.register "image/png", :png
     
     Songtype.all.each do |type|
       Mime::Type.register type.mime_type, type.identifier
