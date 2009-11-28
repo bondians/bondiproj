95867610c46783fe69bf4d0b40aa5ce18fe0b02b
tindrum <tindrum@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2009-11-28 23:09:56 +0000

    making new xserve install
    git-svn-id: svn://localhost/bondiproj@914 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- uva/trunk/config/routes.rb
+++ uva/trunk/config/routes.rb
@@ -58,5 +58,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   
-  map.root :controller => "jobs", :action => "index" , :show => "incomplete" 
+  map.root :controller => "jobs", :action => "index" #, :show => "incomplete" 
 end
