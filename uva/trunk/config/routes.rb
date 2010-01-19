eddd8dfc1ede925214984f287f1b9ded6fb610d9
tindrum <tindrum@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-01-19 23:53:52 +0000

    dave's faster index views
    git-svn-id: svn://localhost/bondiproj@1009 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- uva/trunk/config/routes.rb
+++ uva/trunk/config/routes.rb
@@ -16,8 +16,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
 
   #map.resources :jobs 
-  map.resources :jobs, :collection => { :design => :get, :copy => :get, :press => :get, :bindery => :get, :ship => :get,
-      :completed => :get, :current => :get, :search => :get }, :member => { :complete_step => :put, :new_from_template => :get }
+  map.resources :jobs, :collection => { :design => :get, :copy => :get, \
+    :press => :get, :bindery => :get, :ship => :get, \
+      :completed => :get, :current => :get, :search => :get, :search_by_school => :get }, \
+      :member => { :complete_step => :put, :new_from_template => :get }
 
   # The priority is based upon order of creation: first created -> highest priority.
 
