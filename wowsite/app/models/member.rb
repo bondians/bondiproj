f7e1397b16085c22568b0a35dfaa3c3df38eab42
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-04-19 22:21:26 +0000

    add url
    git-svn-id: svn://localhost/bondiproj@1059 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- wowsite/app/models/member.rb
+++ wowsite/app/models/member.rb
@@ -1,2 +1,3 @@
 class Member < ActiveRecord::Base
+  MEMBER_URL_PREFIX = "http://www.wowarmory.com/character-sheet.xml?"
 end
--- wowsite/db/schema.rb
+++ wowsite/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100416221039) do
+ActiveRecord::Schema.define(:version => 20100419221951) do
 
   create_table "goldberg_content_pages", :force => true do |t|
     t.string   "title"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(:version => 20100416221039) do
     t.integer  "intellect"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "plugin_schema_migrations", :id => false, :force => true do |t|
