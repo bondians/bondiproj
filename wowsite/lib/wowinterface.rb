723a8c5f43fcb37cc5148586d57ffc1f3b50dd3a
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-04-20 15:18:13 +0000

    stuff
    git-svn-id: svn://localhost/bondiproj@1067 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- wowsite/lib/wowinterface.rb
+++ wowsite/lib/wowinterface.rb
@@ -3,7 +3,7 @@ class WowrInterface < Wowr::API
   def initialize
   return super(:character_name => 'Cayuse',
                             :guild_name => "Alliance Pwners II",
-                            :realm => 'Coilfang'
+                            :realm => 'Coilfang')
   end
   
 end
