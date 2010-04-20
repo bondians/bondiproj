f7a570e429705bc2d04011cdb7d398e57bf45662
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-04-20 20:58:27 +0000

    working on library
    git-svn-id: svn://localhost/bondiproj@1072 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- wowsite/lib/wowinterface.rb
+++ wowsite/lib/wowinterface.rb
@@ -1,14 +1,53 @@
 class WowInterface
   
   REALM = 'Coilfang'
+  GUILDS = ["Alliance Pwners", "Alliance Pwners II"]
+  MINIMUMLEVEL = 80
+  
   
   def initialize
-    @api = Wowr::API.new
+    @api = Wowr::API.new (:realm => WowInterface::Realm)
   end
   
   def search(name)
     results = @api.search_characters name
     results.select {|dude| dude.realm == WowInterface::REALM}
+  end
+  
+  def getAttributes(data)
+    attributes = {}
+    attributes[:name] = data.name
+    attributes[:level] = data.level
+    attributes[:racepic] = data.race_icon.gsub("images","_images")
+    attributes[:classpic] = data.class_icon.gsub("images","_images")
+    attributes[:guild] = data.guild
+    attributes[:health] = data.health
+    attributes[:armor] = data.defenses.armor.effective
+    attributes[:strength] = data.str.effective
+    attributes[:stamina] = data.sta.effective
+    attributes[:agility] = data.agi.effective
+    attributes[:spirit] = data.spi.effective
+    attributes[:intellect] = data.int.effective
+    return attributes
+  end
+  
+  def updateMembers
+    results = {:added => 0, :removed => 0, :moved => 0}
+    armoryData = {}
+    WowInterface::GUILDS.each do |guild|
+      @api.guild_name = guild
+      g = api.get_guild guild, :options => {:cache => false}
+      armoryData.merge!(g.members.reject {|k,v| v.level < WowInterface::MINIMUMLEVEL})
+    end
+    
+    members = Member.all
+    armoryData.each do |key,value|
+      attributes = getAttributes value
+      member = members.find {|mem| mem.name == key} || Member.new
+      members.delete member
+      member.update_attributes attributes
+    end
+    
   end
   
 end
