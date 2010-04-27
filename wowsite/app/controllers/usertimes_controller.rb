6a8a4fecfed80ea1766e784e890e6e019dd63643
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-04-27 16:41:50 +0000

    going to freeze gems
    git-svn-id: svn://localhost/bondiproj@1125 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- wowsite/app/controllers/usertimes_controller.rb
+++ wowsite/app/controllers/usertimes_controller.rb
@@ -1,6 +1,5 @@
 class UsertimesController < ApplicationController
-  # GET /usertimes
-  # GET /usertimes.xml
+
   def index
     usertimes = Goldberg.user.usertimes
     @times = []
@@ -10,24 +9,6 @@ class UsertimesController < ApplicationController
     end
   end
 
-  # GET /usertimes/1
-  # GET /usertimes/1.xml
-  def show
-    @usertime = Usertime.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @usertime }
-    end
-  end
-
-  # GET /usertimes/new
-  # GET /usertimes/new.xml
-  def new
-  end
-
-  # GET /usertimes/1/edit
-  # GET /students/1/edit
   def edit
     if params[:with][:id].to_i > 0
       @usertime = (Usertime.find params[:with][:id])
@@ -63,49 +44,4 @@ class UsertimesController < ApplicationController
     
   end
 
-  # POST /usertimes
-  # POST /usertimes.xml
-  def create
-    @usertime = Usertime.new(params[:usertime])
-
-    respond_to do |format|
-      if @usertime.save
-        flash[:notice] = 'Usertime was successfully created.'
-        format.html { redirect_to(@usertime) }
-        format.xml  { render :xml => @usertime, :status => :created, :location => @usertime }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @usertime.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /usertimes/1
-  # PUT /usertimes/1.xml
-  def update
-    @usertime = Usertime.find(params[:id])
-
-    respond_to do |format|
-      if @usertime.update_attributes(params[:usertime])
-        flash[:notice] = 'Usertime was successfully updated.'
-        format.html { redirect_to(@usertime) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @usertime.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /usertimes/1
-  # DELETE /usertimes/1.xml
-  def destroy
-    @usertime = Usertime.find(params[:id])
-    @usertime.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(usertimes_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
