7ae1bcd8442d31a28ac7c44b219f1506ac2dc174
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-05-05 23:14:18 +0000

    teams working
    git-svn-id: svn://localhost/bondiproj@1160 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- wowsite/app/controllers/teams_controller.rb
+++ wowsite/app/controllers/teams_controller.rb
@@ -44,15 +44,18 @@ class TeamsController < ApplicationController
   # POST /teams
   # POST /teams.xml
   def create
+    params[:team][:member_ids] ||= []
     @team = Team.new(params[:team])
-
+    
     respond_to do |format|
       if @team.save
         flash[:notice] = 'Team was successfully created.'
-        format.html { redirect_to(@team) }
+        format.html { redirect_to '/teams' }
         format.xml  { render :xml => @team, :status => :created, :location => @team }
       else
-        debugger;1;1
+        #@unused = (Member.all - Member.find(params[:team][:member_ids]))
+        @unused = Member.all
+        @capabilities = Capability.all
         format.html { render :action => "new" }
         format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
       end
@@ -63,14 +66,16 @@ class TeamsController < ApplicationController
   # PUT /teams/1.xml
   def update
     #params[:member_ids]
-    @team = Team.find(params[:id])
+    @team = Team.find params[:id], :include => :members
 
     respond_to do |format|
       if @team.update_attributes(params[:team])
         flash[:notice] = 'Team was successfully updated.'
-        format.html { redirect_to(@team) }
+        format.html { redirect_to '/teams' }
         format.xml  { head :ok }
       else
+        @capabilities = Capability.all
+        @unused = Team.not_on_team(@team)
         format.html { render :action => "edit" }
         format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
       end
