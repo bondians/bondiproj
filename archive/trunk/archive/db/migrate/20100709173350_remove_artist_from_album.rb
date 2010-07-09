0ac6c33f3257a39a99e8c3332149c4fe9e39f106
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-07-09 17:03:13 +0000

    artist fixes
    git-svn-id: svn://localhost/bondiproj@1210 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/db/migrate/20100709173350_remove_artist_from_album.rb
+++ archive/trunk/archive/db/migrate/20100709173350_remove_artist_from_album.rb
@@ -1,9 +1,9 @@
 class RemoveArtistFromAlbum < ActiveRecord::Migration
   def self.up
-    remove_column :albums, :artist
+    remove_column :albums, :artist_id
   end
 
   def self.down
-    add_column :albums, :artist, :integer
+    add_column :albums, :artist_id, :integer
   end
 end
