37995031287762355590bb23ce03f61f4a534866
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-09-29 23:17:04 +0000

    working on exporter
    git-svn-id: svn://localhost/bondiproj@1239 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/script/jukesetup.rb
+++ archive/trunk/archive/script/jukesetup.rb
@@ -18,9 +18,24 @@ system "rm db/jukebox.sqlite3"
 system "rake goldberg:migrate RAILS_ENV='jukebox'"
 system "rake db:migrate RAILS_ENV='jukebox'"
 
+file = File.open(__FILE__ + "/commands.txt", "wb")
+file.printf(".separator '\t'")
+
+#pg_dump -a mp3 -t genres > genres.db.out
+#./cleaner.pl genres.db.out genres.db
+
+SUDO = "sudo -u cayuse"
+COMMAND = "pg_dump -a"
+DB = (__FILE__ + "/../db/jukebox.sqlite3")
+CLEANER = (__FILE__ + "/cleaner.pl")
+
 
 tables = ActiveRecord::Base.connection.tables
+cmd = sprintf("%s %s %s -t %s > %s.tmp.input", SUDO, COMMAND, DB, table, table )
+system cmd
+secondcmd = sprintf("%s %s.tmp.input %s.input", CLEANER, table, table)
 
+file.printf(".import %s.input %s\n", table, table)
 tables.each do |table|
-  puts table
+  file.printf(".import")
 end
