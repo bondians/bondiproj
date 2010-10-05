7b913e225f6c63ed651834ad2ba8b2d8db9f23b7
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-05 22:10:13 +0000

    only refresh the required data
    git-svn-id: svn://localhost/bondiproj@1289 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- cocoaJukebox/branches/juksite/app/controllers/selections_controller.rb
+++ cocoaJukebox/branches/juksite/app/controllers/selections_controller.rb
@@ -4,6 +4,12 @@ class SelectionsController < ApplicationController
     @randsongs = randlist.map{|a| a.song}
     reqlist = Reqlist.all :order => :sort
     @reqsongs = reqlist.map{|p| p.song.form_idx = p.id; p.song}
-
+    
+    respond_to do |format|
+      format.html #index.html.erb
+      format.js   #index.js.rjs
+    end
+    
   end
+  
 end
