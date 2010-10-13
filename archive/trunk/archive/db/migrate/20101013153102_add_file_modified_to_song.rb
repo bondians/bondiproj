22ce6f7b1de94ed351241bd1acd673016c4e60c4
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-13 15:53:31 +0000

    datetime
    git-svn-id: svn://localhost/bondiproj@1343 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/db/migrate/20101013153102_add_file_modified_to_song.rb
+++ archive/trunk/archive/db/migrate/20101013153102_add_file_modified_to_song.rb
@@ -1,7 +1,9 @@
 class AddFileModifiedToSong < ActiveRecord::Migration
   def self.up
+    add_column :songs, :filemod, :datetime
   end
 
   def self.down
+    remove_column :songs, :filemod
   end
 end
