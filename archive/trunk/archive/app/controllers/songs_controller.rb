e3583291797aa5364950fda4ea373c6f78d353dc
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-15 16:48:45 +0000

    edit/destroy
    git-svn-id: svn://localhost/bondiproj@1359 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/songs_controller.rb
+++ archive/trunk/archive/app/controllers/songs_controller.rb
@@ -4,7 +4,7 @@ class SongsController < ApplicationController
   
   def index
     if params[:search]
-      @songs = Song.search params[:search], :include => [:songtype, :album, :artist, :genre], 
+      @songs = Song.search params[:search], :include => [:songtype, :album, :artist, :genre, :plentries], 
       :order => order_with_default("title", "asc") , :page => params[:page], :per_page => 100
     
       respond_to do |format|
