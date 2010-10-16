3356e21a1ff8f32f3e74006de1f0ddad8f4ef572
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-16 04:45:40 +0000

    fixing stuff?
    git-svn-id: svn://localhost/bondiproj@1392 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/models/album.rb
+++ archive/trunk/archive/app/models/album.rb
@@ -1,6 +1,5 @@
 class Album < ActiveRecord::Base
   has_many :songs
-  has_many :artists, :through => :songs
   
   define_index do
     indexes :name, :sortable => true
@@ -10,6 +9,10 @@ class Album < ActiveRecord::Base
   
   def genre
     self.songs.map{|song| song.genre}.first
+  end
+  
+  def artists
+    self.songs.map{|song| song.artist}.uniq
   end
   
 end
