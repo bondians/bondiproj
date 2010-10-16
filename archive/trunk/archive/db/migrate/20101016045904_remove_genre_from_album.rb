da73024a070a1002a07096a183c19baa75895c01
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-16 05:00:44 +0000

    genre
    git-svn-id: svn://localhost/bondiproj@1395 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/db/migrate/20101016045904_remove_genre_from_album.rb
+++ archive/trunk/archive/db/migrate/20101016045904_remove_genre_from_album.rb
@@ -1,7 +1,9 @@
 class RemoveGenreFromAlbum < ActiveRecord::Migration
   def self.up
+    remove_column :albums, :genre_id
   end
 
   def self.down
+    add_column :albums, :genre_id, :integer
   end
 end
