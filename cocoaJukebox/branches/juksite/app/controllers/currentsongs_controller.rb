22af7e8a3e9a59fdec08174d31ada2a0139d011c
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2010-10-26 06:42:48 +0000

    foo
    git-svn-id: svn://localhost/bondiproj@1485 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- cocoaJukebox/branches/juksite/app/controllers/currentsongs_controller.rb
+++ cocoaJukebox/branches/juksite/app/controllers/currentsongs_controller.rb
@@ -1,2 +1,7 @@
 class CurrentsongsController < ApplicationController
+    def index
+    params[:page] ||= 1
+    playentries = Currentsong.paginate :page => params[:page], :order => 'id DESC'
+    @songs = playentries.select{|s| s.song}
+  end
 end
