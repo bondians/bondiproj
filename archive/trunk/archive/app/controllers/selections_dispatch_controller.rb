a86d6154b4abde3bc1e52afc8f42bcb3cb360291
cayuse <cayuse@2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c>
2011-02-25 22:58:32 +0000

    tar
    git-svn-id: svn://localhost/bondiproj@1612 2f9c37dc-1a64-0410-9d15-b29d5ae7ab4c

--- archive/trunk/archive/app/controllers/selections_dispatch_controller.rb
+++ archive/trunk/archive/app/controllers/selections_dispatch_controller.rb
@@ -13,7 +13,7 @@ class SelectionsDispatchController < ApplicationController
     when "Get As Tar"
       files = songs.collect {|s| Song.find(s).file.gsub(/^[\/]/,"").gsub(" ","\\ ")}
       #files = songs.collect {|s| Song.find(s).file.gsub(/^[\/]/,"")}
-      send_data(`/bin/tar cvhfs - #{files.join " "}`, :filename => 'songs.tar', :type => :tar)
+      send_data(`/bin/tar cvhfs - #{files.join " "}`, :filename => 'songs.tar', :type => :tar, :x_send_file => true)
       #send_data(`/bin/tar cvhfs - \"#{files.join "\" \""}\""`, :filename => 'songs.tar', :type => :tar)
       #send_data( data, :filename => 'songs.tar', :type => :tar)
       #redirect_to(playlist)
